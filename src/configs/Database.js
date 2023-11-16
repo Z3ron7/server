e86ae4a17fa97607306ed1246b1aa97bc8db8476
Zoren Panilagao <89075806+Z3ron7@users.noreply.github.com>
2023-11-16 15:46:37 +0800

    Update Database.js

--- src/configs/Database.js
+++ src/configs/Database.js
@@ -1,21 +1,29 @@
-const mysql = require("mysql2");
-require("dotenv/config");
+const mysql = require('mysql2');
+require('dotenv/config');
+
 const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME } = process.env;
+
 class Database {
   constructor() {
-    this.connection = mysql.createConnection({
+    this.pool = mysql.createPool({
       host: DB_HOST,
       user: DB_USER,
       password: DB_PASSWORD,
       database: DB_NAME,
+      waitForConnections: true,
+      connectionLimit: 10, // Adjust this based on your needs
+      queueLimit: 0,
     });
   }
 
-  TestConnection() {
-    this.connection.connect((err) => {
-      if (err) return err;
-      console.log("Database connected");
-    });
+  async testConnection() {
+    try {
+      const promisePool = this.pool.promise();
+      const [rows, fields] = await promisePool.query('SELECT 1');
+      console.log('Database connected');
+    } catch (error) {
+      console.error('Error connecting to the database:', error);
+    }
   }
 }
 
