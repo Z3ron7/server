f91e7bdc9c0358cecfd3f22f7389bb15b2cc92f4
Zoren Panilagao <89075806+Z3ron7@users.noreply.github.com>
2023-11-17 14:25:17 +0800

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ['http://localhost:3000'],
+    origin: ['https://smartexamhub.vercel.app'],
     methods: ['POST', 'GET'], // Use "methods" instead of "method"
     allowedHeaders: [
       'Origin',
