3563a6dda04412595f96a2e30d53c16077482aa8
Zoren Panilagao <89075806+Z3ron7@users.noreply.github.com>
2023-11-18 12:59:05 +0800

    Update server.js

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.use(
     origin: ['https://smartexamhub.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: [
+      'Origin',
       'X-Requested-With',
       'Content-Type',
       'Accept',
