634497bf3dbca04e483ef9415d970fdc17530ddb
Zoren Panilagao <89075806+Z3ron7@users.noreply.github.com>
2023-11-21 05:22:02 +0800

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(cors(
  {
- origin: ['https://smartexamhub.vercel.app', 'smartexamhub-iyaz0ergk-zoren-panilagaos-projects.vercel.app'],
+ origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
 };
