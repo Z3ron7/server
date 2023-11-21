465ec52d27e408ba4ae8b2ff3bd78cc834296053
Zoren Panilagao <89075806+Z3ron7@users.noreply.github.com>
2023-11-21 12:43:37 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const express = require("express");
 const Database = require("./src/configs/Database");
+const express = require("express");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const cookieParser = require("cookie-parser");
