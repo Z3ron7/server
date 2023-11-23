289b35572c79a8440b1e748851b5dcd58eba513e
Zoren Panilagao <zoren.panilagao1@gmail.com>
2023-11-24 00:01:53 +0800

    server

--- src/routes/Dashboard.js
+++ src/routes/Dashboard.js
@@ -7,28 +7,44 @@ const db = new Database();
 const conn = db.pool;
 
 const queryAsync = promisify(conn.query).bind(conn);
+const jwt = require('jsonwebtoken'); // Make sure to install the 'jsonwebtoken' package
 
-router.get('/fetch-latest', async (req, res) => {
-  const user_id = req.query.userId; // Use req.query to access the query parameter
+// Assuming you have a middleware to check and verify the user's token
+// You should replace this with your own authentication middleware
+const authenticateUser = (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    return res.status(401).json({ message: 'Unauthorized' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, 'your_secret_key'); // Replace 'your_secret_key' with your actual secret key
+    req.user_id = decoded.user_id;
+    next();
+  } catch (error) {
+    console.error('Error verifying token:', error);
+    res.status(401).json({ message: 'Unauthorized' });
+  }
+};
+router.get('/fetch-latest', authenticateUser, async (req, res) => {
+  const user_id = req.user_id; // Access the user_id from the authenticated request
   const limit = parseInt(req.query.limit, 10) || 1; // Default limit is 1 if not specified
 
   try {
-    // Define a SQL query to fetch the latest activity from both tables, limited to the specified limit
     const query = `
-      SELECT * FROM (SELECT user_id, program_id, competency_id, 
-                    end_time, score 
-                    FROM user_exams
-                    WHERE user_id = ?
-                    UNION
-                    SELECT user_id, program_id, competency_id, 
-                    end_time, score 
-                    FROM exam_room) AS combined
+      SELECT * FROM (
+        SELECT user_id, program_id, competency_id, end_time, score 
+        FROM user_exams
+        WHERE user_id = ?
+        UNION
+        SELECT user_id, program_id, competency_id, end_time, score 
+        FROM exam_room
+      ) AS combined
       ORDER BY end_time DESC
       LIMIT ?;`;
   
     const latestActivity = await queryAsync(query, [user_id, limit]);
-
-    // Set the score variable to the score field of the first record
     const score = latestActivity.length > 0 ? latestActivity[0].score : null;
 
     res.json({ latestActivity, score });
