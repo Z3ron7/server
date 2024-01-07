e46e6f649a89a6398501265a8da590d05b3438b8
Zoren <zoren.panilagao1@gmail.com>
2024-01-08 00:58:08 +0800

    updated

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const crypto = require('crypto');
 
 const app = express();
 const corsOptions = {
-  origin: "http://localhost:3000",
+  origin: "https://smartexam.cyclic.app",
   methods: "GET,PUT,POST,DELETE",
   credentials: true,
   optionsSuccessStatus: 204,
@@ -22,9 +22,9 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-app.options("http://localhost:3000", (req, res) => {
+app.options("https://smartexam.cyclic.app", (req, res) => {
   console.log('Request received:', req.method, req.url);
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", "https://smartexam.cyclic.app");
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type");
   res.header("Access-Control-Allow-Credentials", "true");
