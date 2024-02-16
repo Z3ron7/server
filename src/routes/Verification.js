2a5dd5ae78a9054bbfa46f990923e472af596300
Zoren Panilagao <89075806+Z3ron7@users.noreply.github.com>
2024-02-16 11:50:53 +0800

    Update Verification.js

--- src/routes/Verification.js
+++ src/routes/Verification.js
@@ -19,9 +19,9 @@ const corsOptions = {
 router.options("https://smartexamhub.vercel.app", (req, res) => {
   console.log('Request received:', req.method, req.url);
   res.header("Access-Control-Allow-Origin", "https://smartexamhub.vercel.app");
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-  res.header("Access-Control-Allow-Credentials", "true");
+res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
+res.header("Access-Control-Allow-Credentials", "true");
   res.status(200).send();
 });
 
